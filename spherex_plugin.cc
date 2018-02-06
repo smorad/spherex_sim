1c17e79c9b9a15f4dee39dc7fba63ddcc6abd671
Steven Morad <smorad@email.arizona.edu>
2018-02-05 19:26:26 -0700

    Change angular velocity through vel executable

--- spherex_plugin.cc
+++ spherex_plugin.cc
@@ -1,54 +1,108 @@
-#ifndef __SPHEREX_PLUGIN_HH_
-#define __SPHEREX_PLUGIN_HH_
+#ifndef _VELODYNE_PLUGIN_HH_
+#define _VELODYNE_PLUGIN_HH_
 
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
 
 namespace gazebo
 {
-    class SphereXPlugin : public ModelPlugin
+  /// \brief A plugin to control a Velodyne sensor.
+  class SphereXPlugin : public ModelPlugin
+  {
+    /// \brief Constructor
+    public: SphereXPlugin() {}
+
+    /// \brief The load function is called by Gazebo when the plugin is
+    /// inserted into simulation
+    /// \param[in] _model A pointer to the model that this plugin is
+    /// attached to.
+    /// \param[in] _sdf A pointer to the plugin's SDF element.
+    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
     {
-        public: SphereXPlugin() {}
-                
-        public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf){
-            std::cerr << "\nThe SphereX plugin is attached to the model" << 
-                _model->GetName() << "]\n";
-        	//Safety check
-            if (_model->GetJointCount() == 0){
-            	std::cerr << "Invalid joint count, Velodyne plugin not loaded\n";
-                return;
-            }
+      std::cerr << "Loading spherex plugin...\n";
+      // Safety check
+      if (_model->GetJointCount() == 0)
+      {
+        std::cerr << "Invalid joint count, plugin not loaded\n";
+        return;
+      }
 
+      // Store the model pointer for convenience.
+      this->model = _model;
 
+      // Get the first joint. We are making an assumption about the model
+      // having one joint that is the rotational joint.
+      this->joint = _model->GetJoints()[0];
 
-			// Store the model pointer for convenience.
-			this->model = _model;
+      // Setup a P-controller, with a gain of 0.1.
+      this->pid = common::PID(0.1, 0, 0);
 
-			// Get the first joint. We are making an assumption about the model
-			// having one joint that is the rotational joint.
-			this->joint = _model->GetJoints()[0];
+      // Apply the P-controller to the joint.
+      this->model->GetJointController()->SetVelocityPID(
+          this->joint->GetScopedName(), this->pid);
 
-			// Setup a P-controller, with a gain of 0.1.
-			this->pid = common::PID(0.1, 0, 0);
+      // Default to zero velocity
+      double velocity = 0;
 
-			// Apply the P-controller to the joint.
-			this->model->GetJointController()->SetVelocityPID(
-				this->joint->GetScopedName(), this->pid);
+      // Check that the velocity element exists, then read the value
+      if (_sdf->HasElement("velocity"))
+        velocity = _sdf->Get<double>("velocity");
 
-			// Set the joint's target velocity. This target velocity is just
-			// for demonstration purposes.
-			this->model->GetJointController()->SetVelocityTarget(
-				this->joint->GetScopedName(), 1.0);
-		}
-	  /// \brief Pointer to the model.
-	  private: physics::ModelPtr model;
+      this->SetVelocity(velocity);
 
-	  /// \brief Pointer to the joint.
-	  private: physics::JointPtr joint;
+      // Create the node
+      this->node = transport::NodePtr(new transport::Node());
+      #if GAZEBO_MAJOR_VERSION < 8
+      this->node->Init(this->model->GetWorld()->GetName());
+      #else
+      this->node->Init(this->model->GetWorld()->Name());
+      #endif
 
-	  /// \brief A PID controller for the joint.
-	  private: common::PID pid;
-	};
-   GZ_REGISTER_MODEL_PLUGIN(SphereXPlugin)
+      // Create a topic name
+      std::string topicName = "~/" + this->model->GetName() + "/vel_cmd";
+      std::cerr << "Publishing to " << topicName << "\n";
+
+      // Subscribe to the topic, and register a callback
+      this->sub = this->node->Subscribe(topicName,
+         &SphereXPlugin::OnMsg, this);
+    }
+
+    /// \brief Set the velocity of the Velodyne
+    /// \param[in] _vel New target velocity
+    public: void SetVelocity(const double &_vel)
+    {
+      // Set the joint's target velocity.
+      this->model->GetJointController()->SetVelocityTarget(
+          this->joint->GetScopedName(), _vel);
+    }
+
+    /// \brief Handle incoming message
+    /// \param[in] _msg Repurpose a vector3 message. This function will
+    /// only use the x component.
+    private: void OnMsg(ConstVector3dPtr &_msg)
+    {
+      this->SetVelocity(_msg->x());
+    }
+
+    /// \brief A node used for transport
+    private: transport::NodePtr node;
+
+    /// \brief A subscriber to a named topic.
+    private: transport::SubscriberPtr sub;
+
+    /// \brief Pointer to the model.
+    private: physics::ModelPtr model;
+
+    /// \brief Pointer to the joint.
+    private: physics::JointPtr joint;
+
+    /// \brief A PID controller for the joint.
+    private: common::PID pid;
+  };
+
+  // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
+  GZ_REGISTER_MODEL_PLUGIN(SphereXPlugin)
 }
 #endif
