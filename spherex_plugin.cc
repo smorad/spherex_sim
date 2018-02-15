f9a3d6096e05f6f15839a28a060d9ad0c76b7929
Steven Morad <smorad@email.arizona.edu>
2018-02-14 21:57:17 -0700

    lidar data from gazebo published to ros

--- spherex_plugin.cc
+++ spherex_plugin.cc
@@ -6,6 +6,11 @@
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/sensors/sensors.hh>
+#include <gazebo-7/gazebo/transport/PublicationTransport.hh>
+#include <gazebo-7/gazebo/transport/Publication.hh>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/LaserScan.h>
 
 namespace gazebo {
     /// \brief A plugin to control a Velodyne sensor.
@@ -71,20 +76,41 @@ namespace gazebo {
 
         void Sweep(ConstVector3dPtr &_msg) {
             // should be in Hz
-            double update_rate = this->lidar_sensor->GetUpdateRate();
-            for (int slice=0; slice<this->slices; slice++){
-                double theta = this->v_angular_res * slice;
-                this->lidar_sensor->SetActive(true);
-            }
-            this->lidar_sensor->SetActive(false);
+            /* double update_rate = this->lidar_sensor->GetUpdateRate();
+             for (int slice=0; slice<this->slices; slice++){
+                 double theta = this->v_angular_res * slice;
+                 this->lidar_sensor->SetActive(true);
+             }
+             this->lidar_sensor->SetActive(false);*/
 
             // Turn lidar on, start capturing data, sweep sensor, turn off
-            
+
 
         }
-        
-        void WriteScan(ConstVector3dPtr &_msg){
-            // write data somewhere
+
+        void WriteScan(ConstLaserScanStampedPtr &_msg) { // const boost::shared_ptr<msgs::RaySensor>
+            // We got a new message from the Gazebo sensor.  Stuff a
+            // corresponding ROS message and publish it.
+            this->SetVelocity(3);
+            sensor_msgs::LaserScan laser_msg;
+            laser_msg.header.stamp = ros::Time(_msg->time().sec(), _msg->time().nsec());
+            //laser_msg.header.frame_id = this->frame_name_;
+            laser_msg.angle_min = _msg->scan().angle_min();
+            laser_msg.angle_max = _msg->scan().angle_max();
+            laser_msg.angle_increment = _msg->scan().angle_step();
+            laser_msg.time_increment = 0; // instantaneous simulator scan
+            laser_msg.scan_time = 0; // not sure whether this is correct
+            laser_msg.range_min = _msg->scan().range_min();
+            laser_msg.range_max = _msg->scan().range_max();
+            laser_msg.ranges.resize(_msg->scan().ranges_size());
+            std::copy(_msg->scan().ranges().begin(),
+                    _msg->scan().ranges().end(),
+                    laser_msg.ranges.begin());
+            laser_msg.intensities.resize(_msg->scan().intensities_size());
+            std::copy(_msg->scan().intensities().begin(),
+                    _msg->scan().intensities().end(),
+                    laser_msg.intensities.begin());
+            this->scan_pub.publish(laser_msg);
         }
 
         void SetupRot() {
@@ -94,8 +120,8 @@ namespace gazebo {
             this->model->GetJointController()->SetVelocityPID(
                     this->joint->GetScopedName(), this->pid);
             double velocity = 0;
-            if (this->sdf->HasElement("velocity"))
-                velocity = this->sdf->Get<double>("velocity");
+            //if (this->sdf->HasElement("velocity"))
+            //    velocity = this->sdf->Get<double>("velocity");
 
             this->SetVelocity(velocity);
 
@@ -106,13 +132,20 @@ namespace gazebo {
         }
 
         void SetupLidar() {
+            if (!ros::isInitialized()) {
+                ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+                        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+                return;
+            }
             this->lidar_sensor = sensors::get_sensor("sensor");
             this->lidar_sensor->SetActive(false);
             std::cerr << "model is " << this->lidar_sensor << "\n";
-            this->lidar_cmd_sub = this->node->Subscribe(lidar_cmd_topic,
-                    &SphereXPlugin::WriteScan, this);
+            //this->lidar_cmd_sub = this->node->Subscribe(lidar_cmd_topic,
+            //        &SphereXPlugin::WriteScan, this);
             this->scan_sub = this->node->Subscribe(scan_topic,
-                    &SphereXPlugin::Sweep, this);
+                    &SphereXPlugin::WriteScan, this);
+            //this->scan_pub = this->node->Advertise<msgs::RaySensor>(this->output_scan_topic);
+            this->scan_pub = this->ros_node.advertise<sensor_msgs::LaserScan>(output_scan_topic, 1000);
 
         }
 
@@ -121,12 +154,14 @@ namespace gazebo {
         int slices = 64;
         double v_angular_res = full_rot / slices;
 
-        
+
         sensors::SensorPtr lidar_sensor;
         transport::NodePtr node;
         transport::SubscriberPtr vel_sub;
         transport::SubscriberPtr lidar_cmd_sub;
         transport::SubscriberPtr scan_sub;
+        ros::NodeHandle ros_node;
+        ros::Publisher scan_pub;
         physics::ModelPtr model;
         sdf::ElementPtr sdf;
         physics::JointPtr joint;
@@ -134,6 +169,8 @@ namespace gazebo {
         std::string rot_cmd_topic = "~/SphereX/vel_cmd";
         std::string lidar_cmd_topic = "~/SphereX/lidar";
         std::string scan_topic = "~/SphereX/SphereX/SphereXMid/sensor/scan";
+        // CANNOT HAVE ~/ prefix or gazebo will overwrite it
+        std::string output_scan_topic = "raw_lidar_scan";
     };
 
     // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
